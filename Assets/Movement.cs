d286df91f1ff30e843b0be834fc20dd1f9c0f5a4
Fernando Esparza <121126071+JexHunter14@users.noreply.github.com>
2024-09-11 17:50:29 -0500

    Visual, Logical Tweaks

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,13 +2,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f; // You can adjust this value in the Unity Inspector
+    public float speed = 5f;
+    private Vector2 screenBounds;
+    private float playerWidth;
+    private float playerHeight;
+
+    void Start()
+    {
+        Camera mainCamera = Camera.main;
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+
+        
+        playerWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        playerHeight = GetComponent<SpriteRenderer>().bounds.extents.y;
+    }
 
     void Update()
     {
+       
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+       
+        Vector3 newPosition = transform.position + new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
 
-        transform.Translate(new Vector2(moveX, moveY) * speed * Time.deltaTime);
+        newPosition.x = Mathf.Clamp(newPosition.x, -screenBounds.x + playerWidth, screenBounds.x - playerWidth);
+        newPosition.y = Mathf.Clamp(newPosition.y, -screenBounds.y + playerHeight, screenBounds.y - playerHeight);
+
+        transform.position = newPosition;
     }
 }
