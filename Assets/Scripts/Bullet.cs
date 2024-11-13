dea141cd7097f067cee91b1e8bed9b3e33bdbc94
Fernando Esparza <121126071+JexHunter14@users.noreply.github.com>
2024-11-12 18:43:07 -0600

    Enemy backupchanges

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -17,19 +17,23 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
+        if (collider.CompareTag("Area"))
         {
-            // Example: Call a method on the player script to handle damage
+            return;
+        }
+
+        if (collider.CompareTag("Player1"))
+        {
             PlayerHealth playerHealth = collider.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
-                playerHealth.TakeDamage(1); // Adjust damage as needed
+                playerHealth.TakeDamage(1);
             }
-            Destroy(gameObject); // Destroy bullet after hitting a player
-            return; // Ignore collisions with players
+            Destroy(gameObject);
+            return; 
         }
 
-        Destroy(gameObject); // Destroy bullet on hit with other objects
+        Destroy(gameObject); 
     }
 
 }
