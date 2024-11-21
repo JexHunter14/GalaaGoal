fcd85316984c39a0d685e58ebf84cd90432cae1f
BR42PED1 <aaron.ratliff02@gmail.com>
2024-11-21 15:54:40 -0600

    Updated Leaderboard.cs

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,12 +1,69 @@
-public void GetLeaderboard()
-{
-    var request = GetLeaderboardRequest
-    {
-        StatisticName = "Wins",
-        StartPosition = 0,
-        MaxResultsCount = 100 // adjust based on number of players
-    };
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Linq;
+using System;
 
-    PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardRetrieved, OnError)
+public class LeaderboardManager : MonoBehaviour
+{
+    [SerializeField] private GameObject leaderboardEntryPrefab;
+    [SerializeField] private Transform leaderboardContainer;
+
+    void Start()
+    {
+        GetLeaderboard();
+    }
+
+    public void GetLeaderboard()
+    {
+        var request = new GetLeaderboardRequest
+        {
+            StatisticName = "wins",
+            StartPosition = 0,
+            MaxResultsCount = 100
+        };
+
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardRetrieved, OnError);
+    }
+
+    private void OnLeaderboardRetrieved(GetLeaderboardResult result)
+    {
+        List<PlayerLeaderboardEntry> entries = result.Leaderboard;
+
+        entries = entries.OrderByDescending(e => e.StatValue)
+                  .ThenByDescending(e => GetPlayerTotalScore(e.PlayFabId))
+                  .ToList();
+
+        DisplayLeaderboard(entries);
+    }
+
+    private int GetPlayerTotalScore(string PlayFabId)
+    {
+        return 0;
+    }
+
+    private void DisplayLeaderboard(List<PlayerLeaderboardEntry> entries)
+    {
+        foreach (Transform child in leaderboardContainer)
+        {
+            Destroy(child.gameObject);
+        }
+        /*
+        foreach (var entry in entries)
+        {
+            var newEntry = Instantiate(leaderboardEntryPrefab, leaderboardContainter);
+            var entryUI = newEntry.GetComponent<LeaderboardEntryUI>();
+            entryUI.SetEntry(entry.DisplayName, entry.StatValue, GetPlayerTotalScore(entry.PlayFabId));
+        }
+        */
+    }
+
+    private void OnError(PlayFabError error)
+    {
+        Debug.LogError("Error retrieving leaderboard: " + error.GenerateErrorReport());
+    }
+
+
 }
 
